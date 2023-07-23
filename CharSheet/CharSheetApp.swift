fbaa552140170007bc2a372260eb83792b00e6eb
Lexi McQueen <alexismcqueen01@gmail.com>
2023-07-23 19:03:39 -0400

    Initial Commit

---
+++ CharSheet/CharSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharSheetApp.swift
+//  CharSheet
+//
+//  Created by Lexi McQueen on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CharSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
